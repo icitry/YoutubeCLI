38a11bc64977da4f09ec9a7cd65e855c0f2ff986
Axinte Calin-Cristian <icitryofficial@gmail.com>
2024-05-12 21:58:22 +0300

    Fix potential IndexError when writing to stdout
    Reinforce if clause to account for potential empty screen data
    when formatting output.
    Related items: issue #7.

--- service/display/screen/BaseScreen.py
+++ service/display/screen/BaseScreen.py
@@ -9,7 +9,7 @@ class BaseScreen:
         self._menu_str = menu_str
 
     def write_to_screen(self, data: str):
-        data = data + '\n' if data[-1] != '\n' else data
+        data = data + '\n' if len(data) > 0 and data[-1] != '\n' else data
 
         clear = '\033c'
         sys.stdout.write(f"{clear}{data}{self._menu_str}\n")
